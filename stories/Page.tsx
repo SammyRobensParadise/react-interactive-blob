d7f4bb5ef8014ba01328f243941432f526056687
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-07-03 20:27:02 -0700

    created eslint config

--- stories/Page.tsx
+++ stories/Page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import { Header } from './Header'
+import Header from './Header'
 import './page.css'
 
-type User = {
+export type User = {
   name: string
 }
 
@@ -40,7 +40,7 @@ export const Page: React.VFC = () => {
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose
-            such data from the "args" of child component stories
+            such data from the &quot;args&quot; of child component stories
           </li>
           <li>
             Assemble data in the page component from your services. You can mock
