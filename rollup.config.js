2ea9a9d0470e5a4826216d4d365f8bb316481286
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-07-22 19:04:59 -0700

    created build target

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,39 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
-import { terser } from 'rollup-plugin-terser';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import { terser } from 'rollup-plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
-const packageJson = require('./package.json');
+const packageJson = require('./package.json')
 
 export default [
   {
-    input: 'src/index.ts',
+    input: 'src/target/index.ts',
     output: [
       {
         file: packageJson.main,
         format: 'cjs',
-        sourcemap: true,
+        sourcemap: true
       },
       {
         file: packageJson.module,
         format: 'esm',
-        sourcemap: true,
-      },
+        sourcemap: true
+      }
     ],
     plugins: [
       peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
-      terser(),
+      terser()
     ],
-    external: ['react', 'react-dom', 'styled-components'],
+    external: ['react', 'react-dom', 'styled-components']
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
-  },
-];
+    plugins: [dts()]
+  }
+]
---
+++ src/target/index.ts
@@ -0,0 +1,4 @@
+import Blob from '../components/blob/blob.component'
+import { BlobInterface } from '../components/blob/blob.types'
+
+export { Blob, BlobInterface }
