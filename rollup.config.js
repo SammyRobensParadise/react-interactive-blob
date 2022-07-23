93780ad7a2b1bfe755287894842a1b0029cdc57f
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-07-22 19:09:11 -0700

    fixed build system

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-import dts from 'rollup-plugin-dts'
 import { terser } from 'rollup-plugin-terser'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
@@ -30,10 +29,5 @@ export default [
       terser()
     ],
     external: ['react', 'react-dom', 'styled-components']
-  },
-  {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
   }
 ]
